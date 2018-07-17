bdf157b28e3c2323165854d3cbd98b541b55fcc8
benjasanchez <bensan@chalmers.se>
2018-07-17 14:05:48 +0200

    fix: take out mismatched protein
    due to a mismatch a protein was getting in the ES curve as a big outlier

--- code/loadData.R
+++ code/loadData.R
@@ -16,6 +16,7 @@ UPS2$amount.fmoles <- UPS2$amount.fmoles*2.2    #fmol in sample (only 2.2 ug wer
 
 ## @knitr loadIBAQdata
 iBAQdata <- read.csv(file = '../data/raw_internal/mq14_int_std_peaklength-2-points_proteinGroups.txt', sep = '\t', header = TRUE)
+iBAQdata$Protein.IDs <- gsub(';P99999ups','',iBAQdata$Protein.IDs)
 
 
 ## @knitr loadSILACdata
