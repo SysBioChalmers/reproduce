0603c96e1b36320703b1bc12ec6b6d6b01d5fcac
benjasanchez <bensan@chalmers.se>
2018-07-18 17:55:45 +0200

    feat: variability plots
    mainly to compare data with Vs without rescaling

--- code/plotData.R
+++ code/plotData.R
@@ -96,3 +96,102 @@ protPlot <- function(data,pattern) {
   return(mean(totProt)*1e6) #average total pg in sample
 }
 
+
+## @knitr plotScatter
+plotScatter <- function(data1,data2,title) {
+  # Remove NA values - zeros - Infs:
+  no_na  <- is.na(data1) + is.na(data2) == 0
+  data1  <- data1[no_na]
+  data2  <- data2[no_na]
+  check1 <- (data1 > 0)*(!is.infinite((data1))) == 1
+  check2 <- (data2 > 0)*(!is.infinite((data2))) == 1
+  data1  <- data1[check1*check2 == 1]
+  data2  <- data2[check1*check2 == 1]
+  # Define colors:
+  FC         <- data2/data1
+  FCm        <- round(median(10^abs(log10(FC))), digits = 2)
+  col_scheme <- ifelse(abs(log10(FC))>log10(2),
+                       ifelse(abs(log10(FC))>1, '#EB2426', '#F3B70D'), '#0F8141')
+  # Plot data:
+  data1   <- log10(data1)
+  data2   <- log10(data2)
+  min_val <- floor(min(c(data1,data2)))
+  max_val <- ceiling(max(c(data1,data2)))
+  plot(data1, data2, pch = 1, col = col_scheme, xlab = '', ylab = '',
+       xaxs = 'i', yaxs = 'i', xaxt = 'n', yaxt = 'n', main = title,
+       xlim = c(min_val,max_val), ylim = c(min_val,max_val))
+  axis(side=1, at = seq(min_val, max_val, by = 1), labels = FALSE, tck = 0.015)
+  axis(side=2, at = seq(min_val, max_val, by = 1), labels = FALSE, tck = 0.015)
+  axis(side=3, at = seq(min_val, max_val, by = 1), labels = FALSE, tck = 0.015)
+  axis(side=4, at = seq(min_val, max_val, by = 1), labels = FALSE, tck = 0.015)
+  abline(0,1,col='black')
+  # Show fit to a linear model:
+  lmodel <- lm(data2 ~ data1)
+  lsum   <- summary(lmodel)
+  R2     <- round(lsum$adj.r.squared,2)
+  text(min_val,max_val-0.5, bquote('R'^2 ~ '=' ~ .(R2)), pos = 4)
+  text(min_val,max_val-1.5, bquote('FC'['m'] ~ '=' ~ .(FCm)), pos = 4)
+}
+
+
+## @knitr plotVariability
+plotVariability <- function(data,groupNames,title) {
+  # Erase distinction from name:
+  for(i in 1:length(groupNames)) {
+    names(data) <- gsub(groupNames[i],'',names(data))
+  }
+  # Create data with all possible combinations:
+  data1 <- NULL
+  data2 <- NULL
+  for(i in 2:(length(names(data))-1)) {
+    for(j in (i+1):length(names(data))) {
+      if(names(data)[i] == names(data)[j]) {
+        data1 <- c(data1,data[,i])
+        data2 <- c(data2,data[,j])
+      }
+    }
+  }
+  # Plot all combinations:
+  plotScatter(data1,data2,title)
+}
+
+
+## @knitr plotPCA
+plotPCA <- function(data,title){
+  # Take log from data:
+  log_data                        <- log10(as.matrix(data))
+  log_data[is.infinite(log_data)] <- NA
+  log_data[is.nan(log_data)]      <- NA
+  log_data                        <- log_data[!is.na(rowSums(log_data)),]
+  log_data                        <- t(log_data)
+  #Do PCA:
+  pca  <- prcomp(log_data)
+  var  <- pca$sdev/sum(pca$sdev)*100
+  var1 <- round(var[1], digits = 1)
+  var2 <- round(var[2], digits = 1)
+  #Plotting options:
+  pch_opt <- NULL
+  col_opt <- NULL
+  for(i in 2:length(names(data))) {
+    #Shape by bio. rep.
+    if(length(grep('R1.1',names(data)[i])) == 1)      { pch_opt[i] <- 1 } 
+    else if(length(grep('R2.1',names(data)[i])) == 1) { pch_opt[i] <- 2 }
+    else if(length(grep('R3.1',names(data)[i])) == 1) { pch_opt[i] <- 3 }
+    #Color by tech. rep.
+    if(length(grep('Batch1',names(data)[i])) == 1)      { col_opt[i] <- '#EB2426' }
+    else if(length(grep('Batch2',names(data)[i])) == 1) { col_opt[i] <- '#3953A3' }
+    else if(length(grep('Batch3',names(data)[i])) == 1) { col_opt[i] <- '#0F8141' }
+  }
+  #Plot PCA:
+  deltax <- max(pca$x[,1]) - min(pca$x[,1])
+  deltay <- max(pca$x[,2]) - min(pca$x[,2])
+  xmin   <- min(pca$x[,1]) - deltax/8
+  xmax   <- max(pca$x[,1]) + deltax/8
+  ymin   <- min(pca$x[,2]) - deltay/8
+  ymax   <- max(pca$x[,2]) + deltay/8
+  plot(pca$x, col = col_opt, pch = pch_opt, cex = 1.5, xaxt = 'n', yaxt = 'n',
+       xlim = c(xmin, xmax), ylim = c(ymin, ymax), main = title)
+  mtext(bquote('PC1 = ' ~ .(var1) ~ '%'), side = 1, line = -1)
+  mtext(bquote('PC2 = ' ~ .(var2) ~ '%'), side = 2, line = -1)
+}
+
