ce8d024826cd2b87db13ef4841a25568ca2d1c21
Benjamin Sanchez <bjsanchezb@gmail.com>
2020-03-16 18:59:50 +0100

    feat: subfigure indexing

--- code/mainFigures.R
+++ code/mainFigures.R
@@ -8,8 +8,11 @@ setEPS()
 postscript("../results/figures/figure1.eps", height = 3.5, width = 8.5)
 par(mfrow = c(1,3), mar = c(4,4,1,1), cex = 0.9)
 plotCumulativeDistrib(ups2FC,'UPS2')
+text(0.05, 0.9, 'A', pos=4, cex=1.5)
 plotCumulativeDistrib(rpFC,'Ribo')
+text(0.05, 0.9, 'B', pos=4, cex=1.5)
 plotCumulativeDistrib(techFC,'Tech')
+text(0.05, 0.9, 'C', pos=4, cex=1.5)
 dev.off()
 
 
@@ -20,9 +23,12 @@ par(mfrow = c(1,3), mar = c(4,4,1,1), cex = 1)
 plotVariability(abundanceTPAN[,-1],c('.R1.1','.R2.1','.R3.1'),'',
                 bquote('log'['10'] ~ '(abundance)'),
                 bquote('log'['10'] ~ '(abundance)'))
+text(-3.5, 3, 'A', pos=4, cex=1.5)
 plotVariability(abundanceTPAN[,-1],c('_batch1','_batch2','_batch3'),'',
                 bquote('log'['10'] ~ '(abundance)'),
                 bquote('log'['10'] ~ '(abundance)'))
+text(-3.5, 3, 'B', pos=4, cex=1.5)
 plotPCA(abundanceTPAN[,-1],'',TRUE)
+text(-6, 3.5, 'C', pos=4, cex=1.5)
 dev.off()
 
