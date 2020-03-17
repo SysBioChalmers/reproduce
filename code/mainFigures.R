3eff35a203395e26e040133040c9f6a116ed3c6d
Benjamin Sanchez <bjsanchezb@gmail.com>
2020-03-17 16:48:31 +0100

    feat: final figures as pdf

--- code/mainFigures.R
+++ code/mainFigures.R
@@ -4,8 +4,7 @@
 
 
 ## @knitr figure1
-setEPS()
-postscript("../results/figures/figure1.eps", height = 3.5, width = 8.5)
+pdf("../results/figures/figure1.pdf", height = 3.5, width = 8.5)
 par(mfrow = c(1,3), mar = c(4,4,1,1), mgp = c(2.7,0.7,0), cex = 0.9)
 plotCumulativeDistrib(ups2FC,'UPS2')
 text(0.05, 0.9, 'A', pos=4, cex = 1.5)
@@ -17,8 +16,7 @@ dev.off()
 
 
 ## @knitr figure2
-setEPS()
-postscript("../results/figures/figure2.eps", height = 3, width = 8.5)
+pdf("../results/figures/figure2.pdf", height = 3, width = 8.5)
 par(mfrow = c(1,3), mar = c(4,4,1,1), mgp = c(1.5,0.6,0), cex = 1)
 plotVariability(abundanceTPAN[,-1],c('.R1.1','.R2.1','.R3.1'),'',
                 bquote('log'['10'] ~ '(abundance)'),
