b7c1de9d0ef893705c7fdedeba2133761df22952
Benjamin Sanchez <bjsanchezb@gmail.com>
2020-09-28 16:20:05 +0200

    style: switch back to eps
    some figure symbols won't load in illustrator as PDF

--- code/mainFigures.R
+++ code/mainFigures.R
@@ -4,7 +4,8 @@
 
 
 ## @knitr figure1
-pdf("../results/figures/figure1.pdf", height = 3.5, width = 8.5)
+setEPS()
+postscript("../results/figures/figure1.eps", height = 3.5, width = 8.5)
 par(mfrow = c(1,3), mar = c(4,4,1,1), mgp = c(2.7,0.7,0), cex = 0.9)
 plotCumulativeDistrib(ups2FC,'UPS2')
 text(0.05, 0.9, 'A', pos=4, cex = 1.5)
@@ -16,7 +17,8 @@ dev.off()
 
 
 ## @knitr figure2
-pdf("../results/figures/figure2.pdf", height = 3, width = 8.5)
+setEPS()
+postscript("../results/figures/figure2.eps", height = 3, width = 8.5)
 par(mfrow = c(1,3), mar = c(4,4,1,1), mgp = c(1.5,0.6,0), cex = 1)
 plotVariability(abundanceIBAQ[,-1],c('.R1.1','.R2.1','.R3.1'),'',
                 bquote('log'['10'] ~ '(abundance)'),
